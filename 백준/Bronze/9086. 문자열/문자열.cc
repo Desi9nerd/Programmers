ea8f2346e6fbeba314b90aa4a0b2bad918885363
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2023-04-09 15:08:50 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+		cout << str[0] << str[str.length() - 1] << endl;
+	}
+}
