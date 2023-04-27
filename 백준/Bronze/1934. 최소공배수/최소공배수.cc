183d08254104238310c24f01258e106fbd8761a8
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2023-04-28 00:07:52 +0900

    [Bronze I] Title: 최소공배수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.cc
+++ 백준/Bronze/1934. 최소공배수/최소공배수.cc
@@ -1,49 +1,18 @@
 #include <stdio.h>
-#include <vector>
-using namespace std;
 
-vector<int> v;
-
-int main() {
-	int t, a, b, cf;
-	scanf("%d", &t);
-
-	for (int i = 0; i < t; i++) 
-	{
-		cf = 1;
-		scanf("%d %d", &a, &b);
-
-		if(a>b)
-		{
-			for (int j = b; j >= 1; j--)
-			{
-				if (a % j == 0 && b % j == 0)
-				{
-					cf *= j; //공약수를 곱해 최대공약수를 구함. 
-					a /= j;
-					b /= j;
-				}
-			}
-		}
-		else
-		{
-			for (int j = a; j >= 1; j--)
-			{
-				if (a % j == 0 && b % j == 0)
-				{
-					cf *= j; //공약수를 곱해 최대공약수를 구함. 
-					a /= j;
-					b /= j;
-				}
-			}
-		}
-
-		v.push_back(a * b * cf);
-	}
-
-	for (int i = 0; i < v.size(); i++)
-		printf("%d\n", v[i]);
-
-	return 0;
+int GCD(int a, int b)
+{
+    return b ? GCD(b, a%b) : a;
 }
 
+int main()
+{
+    int T, A, B;
+    scanf("%d", &T);
+    
+    while (T--)
+    {
+        scanf("%d %d", &A, &B);
+        printf("%d\n", A*B / GCD(A, B));
+    }
+}
