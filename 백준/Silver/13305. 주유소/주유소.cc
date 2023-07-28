0d1663aec137480d98c57da1f4844a54e1e42732
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2023-07-28 16:02:43 +0900

    [Silver III] Title: 주유소, Time: 20 ms, Memory: 3196 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -8,9 +8,11 @@ vector<int> r;
 vector<long long> c;
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
 
 	cin >> n;
-	r.resize(n);
+	r.resize(n-1);
 	for (int i = 0; i < n - 1; i++) {
 		cin >> r[i];
 	}
@@ -22,9 +24,9 @@ int main() {
 
 	for(int i = 0; i < n; i++)
 	{
-		tmp = n;
+		tmp = n-1;
 		//가스 값이 더 싼 도시 찾기
-		for(int j=i+1; j < n; j++){
+		for(int j=i; j < n; j++){
 			if(c[i] > c[j]){
 				tmp = j;
 				break;
@@ -33,14 +35,18 @@ int main() {
 
 		//다음 도시까지의 길이
 		long long nextCity = 0;
-		for(int k = i; k < tmp; k++){
+		for (int k = i; k < tmp; k++) {
 			nextCity += r[k];
 		}
-		 
+		
+
 		//다음 도시까지 필요한 가스를 충전. 현재 도시 가스가격 * 다음 도시까지의 길이
 		minCost += c[i] * nextCity;
 
-		i += (tmp - 1);
+		if (tmp < n - 1)
+			i = (tmp - 1);
+		else
+			i = n - 1;
 	}
 
 	cout << minCost;
