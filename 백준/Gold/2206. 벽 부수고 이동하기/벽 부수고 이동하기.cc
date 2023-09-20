580eb77d9cc52a177d54bb570552f8a93ca99a68
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2023-09-20 13:44:04 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 304 ms, Memory: 62724 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -12,9 +12,9 @@ int dirX[4] = { 0, -1, 0, 1 };
 struct Pos{
 	int y;
 	int x;
-	int wallbreak;
+	int wallbreak; // 벽을 부셨는지 여부 (부셨으면 1 아니면 0)
 };
-queue<Pos> Q; // BFS 큐, 각 pair는 (y, x) 좌표
+queue<Pos> Q; // BFS 큐, (y, x) 좌표와 벽을 부셨는지 여부를 담음
 vector<vector<vector<int>>> check;; // 각 위치에서 벽 부셨는지 확인하는 배열
 
 int main()
@@ -23,6 +23,8 @@ int main()
 	cin >> n >> m;
 	v.resize(n+2, vector<int>(m+2));
 	check.resize(n + 2, vector<vector<int>>(m + 2, vector<int>(2)));
+
+	//입력을 받아서 맵 구성하기
 	for (int y = 1; y <= n; y++) {
 		string input;
 		cin >> input;
@@ -32,7 +34,7 @@ int main()
 	}
 	
 	Q.push({ 1, 1, 0 }); // 시작점
-	check[1][1][0] = 1;
+	check[1][1][0] = 1; //시작점 체크
 
 	int answer = 1; // 시작하는 칸도 카운팅
 
@@ -60,16 +62,16 @@ int main()
 
 				if (ny < 1 || n < ny || nx < 1 || m < nx) continue; //맵 벗어나는 경우
 
-				//이전에 벽을 부순적X, 벽인 경우
+				// 벽인 경우, 이전에 벽을 부순적X (w==0)
 				if (v[ny][nx] == 1 && w == 0)
 				{
-					Q.push({ ny, nx, 1 }); // 새로운 위치 Q에 넣기
-					check[ny][nx][1] = 1; //
+					Q.push({ ny, nx, 1 }); // 새로운 위치 Q에 넣기, 1값을 넣어 벽을 부섰음을 체크
+					check[ny][nx][1] = 1; //[1]벽 부순걸 체크, =1로 해당 위치 방문 체크
 				}
-				//이전에 벽을 부순적X, 벽이 아닌 경우
+				// 벽이 아닌 경우, 방문한 적이 없는 경우(check[ny][nx][w]==0)
 				else if (v[ny][nx] == 0 && check[ny][nx][w] == 0) {
 					Q.push({ ny, nx, w }); // 새로운 위치 Q에 넣기
-					check[ny][nx][w] = 1; //		
+					check[ny][nx][w] = 1; // =1로  해당 위치 방문 체크		
 				}
 			}
 		}
