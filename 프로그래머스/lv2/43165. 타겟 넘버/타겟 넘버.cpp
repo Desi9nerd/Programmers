7db68a2f89e823b6c23859b520cb972bade43055
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2023-09-25 10:25:35 +0900

    [level 2] Title: 타겟 넘버, Time: 0.16 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -1,26 +1,52 @@
 #include <string>
 #include <vector>
+#include <queue>
 using namespace std;
 
 int answer; // 타겟 넘버를 만드는 방법의 수
-int ch[21]; // 방문 체크 배열
 
-void DFS(int x, int sum, int target, vector<int>& numbers)
+queue<pair<int,int>> Q;
+
+//void DFS(int x, int sum, int target, vector<int>& numbers)
+//{
+//    if(x == numbers.size()){
+//        if(sum == target){
+//            answer++;
+//        }
+//        return;
+//    }
+//       
+//    DFS(x + 1, sum + numbers[x], target, numbers);
+//    DFS(x + 1, sum - numbers[x], target, numbers);   
+//}
+
+void BFS(vector<int>& nums, int target)
 {
-    if(x == numbers.size()){
-        if(sum == target){
+    Q.push({ 0,-nums[0] });   // - 넣기
+    Q.push({ 0,nums[0] }); // + 넣기
+
+    while (!Q.empty()) {
+        int idx = Q.front().first;
+        int total = Q.front().second;
+        Q.pop();
+
+        if (idx == nums.size() - 1 && total == target) {
             answer++;
+            continue;
         }
-        return;
+        if (idx == nums.size() - 1 && total != target) {
+            continue;
+        }
+               
+        Q.push({ idx + 1, total + (-1 * nums[idx + 1]) });
+        Q.push({ idx + 1, total + (nums[idx + 1]) });
     }
-       
-    DFS(x + 1, sum + numbers[x], target, numbers);
-    DFS(x + 1, sum - numbers[x], target, numbers);   
 }
 
 int solution(vector<int> numbers, int target) {
   
-    DFS(0, 0, target, numbers);
+    //DFS(0, 0, target, numbers);
+    BFS(numbers, target);
 
     return answer;
 }
