998606cace18b22b3d3a8cee2b19ae7a466c3775
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2023-10-12 11:26:47 +0900

    [level 2] Title: 방문 길이, Time: 0.02 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.cpp
@@ -1,53 +1,46 @@
 #include <string>
 using namespace std;
 
-int answer = 0;  // 방문 길이
-int chPath[11][11][11][11]; 
-int y = 0, x = 0; // 시작 위치
-
-// 방문 체크 후 방문한 적이 없으면 길이 늘리기
-void checkPath(int prevY, int prevX, int y, int x) 
-{
-	if(chPath[prevY + 5][prevX + 5][y + 5][x + 5] == 0
-        && chPath[y + 5][x + 5][prevY + 5][prevX + 5] == 0)
-	{
-        chPath[prevY + 5][prevX + 5][y + 5][x + 5] = 1;
-        answer++;
-        chPath[y + 5][x + 5][prevY + 5][prevX + 5] = 1;
-	}
-
-    return;
-}
-
-void Move(char d, int& y, int& x)
-{
-    int prevY = y;
-    int prevX = x;
-
-    // 이동
-    if (d == 'U' && y < 5){
-        y = y + 1;
-        checkPath(prevY, prevX, y, x);
-    }
-    else if (d == 'D' && -5 < y) {
-        y = y - 1;
-        checkPath(prevY, prevX, y, x);
-    }
-    else if (d == 'R' && x < 5) {
-        x = x + 1;
-        checkPath(prevY, prevX, y, x);
-    }
-    else if (d == 'L' && -5 < x) {
-        x = x - 1;
-        checkPath(prevY, prevX, y, x);
-    }    
-}
+int dy[4] = {1, -1, 0, 0};
+int dx[4] = {0, 0, -1, 1};
+bool visited[11][11][11][11];
 
 int solution(string dirs) {
-    
-    for(int i=0; i<dirs.size(); i++)
-    {
-        Move(dirs[i], y, x);
+    int answer = 0;
+    int x = 5;
+    int y = 5;
+
+    for(char dir : dirs){
+        int idx;
+        switch(dir){
+            case 'U':
+                idx=0;
+                break;
+            case 'D':
+                idx=1;
+                break;            
+            case 'R':
+                idx=2; 
+                break;            
+            case 'L':
+                idx=3; 
+                break;  
+        }
+
+        int nx=x+dx[idx];
+        int ny=y+dy[idx];
+
+        if(nx>=0 && nx<=10 && ny>=0 && ny<=10){
+            if(!visited[x][y][nx][ny]){
+               visited[x][y][nx][ny]=true;
+               visited[nx][ny][x][y]=true; //반대 방향도 체크
+               answer++;
+            }
+            
+            x=nx;
+            y=ny;
+        }
+        
     }
 
     return answer;
