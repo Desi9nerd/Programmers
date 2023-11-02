fc78f009bf3264e78ba7dfb14cec964dc99186cc
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2023-11-02 09:41:40 +0900

    [level 2] Title: 숫자 변환하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.cpp
@@ -1,35 +1,35 @@
+#include <iostream>
 #include <queue>
 #include <vector>
 using namespace std;
 
 int solution(int x, int y, int n) {
-    vector<int> visited(1000001, -1); // 방문 체크 및 연산 횟수를 저장할 배열
-    queue<int> q;
 
-    visited[y] = 0; // 시작점의 연산 횟수는 0
-    q.push(y);
+	int answer = -1;
+	queue<pair<int,int>> Q; // 위치, 연산 횟수
 
-    while (!q.empty()) {
-        int cur = q.front();
-        q.pop();
+	Q.push({ y, 0 });
+	while(!Q.empty())
+	{
+		int num = Q.front().first;
+		int cnt = Q.front().second;
+		Q.pop();
 
-        if (cur == x) // x에 도달한 경우
-            return visited[cur];
+		if (num == x){
+			answer = cnt;
+			break;
+		}
 
-        // 가능한 모든 연산을 수행
-        if (cur - n >= 0 && visited[cur - n] == -1) {
-            q.push(cur - n);
-            visited[cur - n] = visited[cur] + 1;
-        }
-        if (cur % 2 == 0 && visited[cur / 2] == -1) {
-            q.push(cur / 2);
-            visited[cur / 2] = visited[cur] + 1;
-        }
-        if (cur % 3 == 0 && visited[cur / 3] == -1) {
-            q.push(cur / 3);
-            visited[cur / 3] = visited[cur] + 1;
-        }
-    }
+		if (num - n >= x) {
+			Q.push({ num - n, cnt + 1 });
+		}
+		if (num % 2 == 0){
+			Q.push({ num / 2, cnt + 1 });
+		}
+		if (num % 3 == 0) {
+			Q.push({ num / 3, cnt + 1 });
+		}
+	}
 
-    return -1; // x에 도달할 수 없는 경우
+	return answer;
 }
