cdfd76050d96a3045be83aa0a2cb5376bb3cc62b
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2023-11-09 11:18:58 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -1,32 +1,25 @@
 #include <string>
-#include <vector>
+#include <deque>
 using namespace std;
 
 string solution(string number, int k) {
-    string answer = "";
-
-    int n = number.size();
-    int remain = n - k; // 남아야하는 숫자 개수
-
-    int start = 0;  // 탐색을 시작할 인덱스
-
-    for (int i = 0; i < remain; i++)  // 남아야 하는 숫자의 개수만큼 반복
-    {
-        char maxNum = number[start];
-        int maxIdx = start;
-
-        // start부터 k+i까지 반복하며 최대 숫자와 그 인덱스 찾기
-        for (int j = start; j <= k + i; j++) 
-        {
-            if (maxNum < number[j]) {
-                maxNum = number[j];
-                maxIdx = j;
-            }
+    deque<char> dq;
+    for (int i = 0; i < number.size(); i++) {
+        while (!dq.empty() && dq.back() < number[i] && k > 0) {
+            dq.pop_back();
+            k--;
         }
+        dq.push_back(number[i]);
+    }
 
-        start = maxIdx + 1; // start를 최대값 숫자의 다음 인덱스로 갱신
+    while (k--) {
+        dq.pop_back();
+    }
 
-        answer += maxNum;  // 최대값 숫자를 결과 문자열에 추가
+    string answer = "";
+    while (!dq.empty()) {
+        answer += dq.front();
+        dq.pop_front();
     }
 
     return answer;
