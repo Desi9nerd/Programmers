38582f5215d439d75791dce8153c8b1f45ca4c5d
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2023-11-14 11:15:42 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 1.90 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.cpp
@@ -9,16 +9,13 @@ int answer;
 int solution(vector<int> queue1, vector<int> queue2) {
 	int n = queue1.size();
 	for(int i=0; i<n; i++){
-		sumNum += queue1[i];
+		sumNum += queue1[i] + queue2[i];
 		sum1 += queue1[i];
 		Q1.push(queue1[i]);
-	}
-	for(int i=0; i<n; i++){
-		sumNum += queue2[i];
 		sum2 += queue2[i];
 		Q2.push(queue2[i]);
 	}
-
+    
 	bool success = false;
 	int maxTrial = 3 * n;
 	while (maxTrial--)
