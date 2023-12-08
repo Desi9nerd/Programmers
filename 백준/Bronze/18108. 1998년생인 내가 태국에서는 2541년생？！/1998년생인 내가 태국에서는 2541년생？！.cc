8bb1f79613ef4a4fcbe7ca6a6822e69c4c2e4eee
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2023-12-08 13:48:17 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int bYear; 
+    int diff = 2541 - 1998; 
+
+    cin >> bYear;                 
+    cout << bYear - diff; 
+
+    return 0;
+}
