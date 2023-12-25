a517889f6827530887e6a1959121d7294e678472
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2023-12-25 20:39:07 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> v;
+vector<int> result;
+int ch[9]; // 선택된 숫자의 사용 여부를 체크하는 배열
+
+void BackT(int idx, int cnt)
+{
+	if(cnt == m){
+		for(int i = 0; i < m; i++){
+			cout << result[i] << " ";
+		}
+		cout << "\n";
+
+		return;
+	}
+
+	for(int i = idx; i < n; i++)
+	{
+		if (ch[i] == 0){
+			ch[i] = 1;
+			result[cnt] = v[i];
+			BackT(i + 1, cnt + 1);
+			ch[i] = 0;
+		}
+	}
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> n >> m;
+	v.resize(n);
+	result.resize(m);
+	for(int i = 0; i < n; i++){
+		cin >> v[i];
+	}
+	sort(v.begin(), v.end());
+
+	BackT(0, 0);
+
+	return 0;
+}
