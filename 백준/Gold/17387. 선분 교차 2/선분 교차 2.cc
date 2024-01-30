c6a869a405e7ae8e8ff70a2e88018361a7d7bcb9
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2024-01-30 12:10:33 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+#define x first
+#define y second
+using int64 = long long;
+using Point = pair<int64, int64>;
+
+int CCW(const Point& a, const Point& b, const Point& c)
+{
+	// 행렬식을 사용해서 외적 구하기
+	long long result = (a.x * b.y + b.x * c.y + c.x * a.y) - (a.y * b.x + b.y * c.x + c.y * a.x);
+
+	if (result > 0) return 1;		 // CCW. 반시계 방향
+	else if (result == 0) return 0; // 일직선
+	else return -1;					 // CW. 시계 방향
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	Point a, b, c, d;
+	cin >> a.x >> a.y;
+	cin >> b.x >> b.y;
+	cin >> c.x >> c.y;
+	cin >> d.x >> d.y;
+
+	// CCW 구하기
+	int ABC = CCW(a, b, c);
+	int ABD = CCW(a, b, d);
+	int CDA = CCW(c, d, a);
+	int CDB = CCW(c, d, b);
+
+	if (ABC * ABD == 0 && CDA * CDB == 0)
+	{
+		if (a > b) swap(a, b);
+		if (c > d) swap(c, d);
+
+		if (a <= d && c <= b) cout << 1;
+		else cout << 0;
+
+		return 0;
+	}
+
+	if (ABC * ABD <= 0 && CDA * CDB <= 0) cout << 1;
+	else cout << 0;
+
+	return 0;
+}
