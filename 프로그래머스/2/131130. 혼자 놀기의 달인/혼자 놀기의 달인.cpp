6728b6b020f53c97149958d8effae47c67bace96
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2024-03-11 10:43:38 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.cpp
+++ 프로그래머스/2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.cpp
@@ -1,45 +1,34 @@
-#include <set>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-int solution(vector<int> cards) {
-    
+int solution(vector<int> cards)
+{
     int n = cards.size();
 
-    if (n < 1) return 0;
+    vector<bool> ch(n + 1, false); // 카드를 골랐는지 체크하는 bool 배열
+    vector<int> scores;                      // 상자에 담긴 카드 수(=점수)를 기록하는 배열
 
-    vector<int> scores;
-    int i = 0;
-    while(n > 0)
+    for (int i = 0; i < n; i++)
     {
-        set<int> mySet;
-        while (true)
+        int now = cards[i];
+    	int cnt = 0;
+        while (false == ch[now]) // 아직 고른 카드가 아니라면
         {
-            if (cards[i] == -1){
-                i++;
-                break;
-            }
-
-        	if (mySet.find(cards[i]) == mySet.end()) {
-                mySet.insert(cards[i]);
-                //cout << cards[i] << " ";
-                int tmp = i;
-                i = cards[tmp] - 1;
-                cards[tmp] = -1;
-                n--;
-            }
-            else {
-                break;
-            }
+            ch[now] = true;        // 선택한걸 체크
+            now = cards[now - 1];  // 현재 카드 업데이트
+            cnt++;                 // 개수++
         }
-        //cout << "\n";
-        scores.push_back(mySet.size());
+
+        if (cnt > 0) {
+            scores.push_back(cnt); // 점수 추가
+        }
     }
 
     if (scores.size() == 1) return 0; // 첫 시도에 모든 상자를 연 경우
 
-    sort(scores.rbegin(), scores.rend());
 
-    return scores[0] * scores[1];
+    sort(scores.rbegin(), scores.rend()); // 내림차순 정렬
+
+	return scores[0] * scores[1];
 }
