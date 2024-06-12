3296ca06f209ac946fddd5e3578c6cac412833e6
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2024-06-12 12:32:55 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.02 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -1,57 +1,48 @@
 #include <string>
 #include <vector>
 #include <queue>
-#include <cstring>
-#include <functional>
-
 using namespace std;
 
-priority_queue<int,vector<int>,greater<int>> min_heap;
-priority_queue<int> max_heap;
-
-vector<int> solution(vector<string> arguments) {
+vector<int> solution(vector<string> oper) {
     vector<int> answer;
 
-    for(int i=0;i<arguments.size();i++)
+    priority_queue<int> pQ;
+    priority_queue<int, vector<int>, greater<int>> revpQ;
+
+    for (int i = 0; i < oper.size(); i++)
     {
-        string a = arguments[i];
-        char o = a[0];
-        int n = atoi(a.substr(2).c_str());
-
-        if(o == 'I')
-        {
-            min_heap.push(n);
-            max_heap.push(n);
+        if (oper[i][0] == 'I') {
+            int num = stoi(oper[i].substr(2));
+            pQ.push(num);
+            revpQ.push(num);
         }
-        else if(o == 'D' && n == 1)
-        {
-            if(max_heap.empty()) continue;
-            if(max_heap.top() == min_heap.top()) min_heap.pop();
-            max_heap.pop();
-        }
-        else if (o == 'D' && n == -1)
-        {
-            if(min_heap.empty()) continue;
-            if(max_heap.top() == min_heap.top()) max_heap.pop();
-            min_heap.pop();
+        else if (oper[i][0] == 'D') {
+            if (oper[i][2] == '1') {
+                if (pQ.empty()) continue;
+                if (!revpQ.empty() && pQ.top() == revpQ.top()) revpQ.pop();
+                pQ.pop();
+            }
+            else if (oper[i][2] == '-') {
+                if (revpQ.empty()) continue;
+                if (!pQ.empty() && pQ.top() == revpQ.top()) pQ.pop();
+                revpQ.pop();
+            }
         }
 
-        if(max_heap.top() < min_heap.top())
+        if (pQ.top() < revpQ.top())
         {
-            while(!min_heap.empty()) min_heap.pop();
-            while(!max_heap.empty()) max_heap.pop();
+            while (!pQ.empty()) pQ.pop();
+            while (!revpQ.empty()) revpQ.pop();
         }
     }
 
-    if(min_heap.empty()) 
-    {
-        answer.push_back(0);
-        answer.push_back(0);
+    if (pQ.empty() || revpQ.empty()) {
+        answer.emplace_back(0);
+        answer.emplace_back(0);
     }
-    else
-    {
-        answer.push_back(max_heap.top());
-        answer.push_back(min_heap.top());
+    else {
+        answer.emplace_back(pQ.top());
+        answer.emplace_back(revpQ.top());
     }
 
     return answer;
