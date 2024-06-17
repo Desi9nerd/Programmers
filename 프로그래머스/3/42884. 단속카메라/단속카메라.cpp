280ff6e94dbbb262f63f25bf073c1363e6cdf0c4
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2024-06-17 11:29:20 +0900

    [level 3] Title: 단속카메라, Time: 0.60 ms, Memory: 3.95 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+bool Compare(vector<int>& v1, vector<int>& v2) {
+    if (v1[1] < v2[1]) return true;
+    if (v1[1] == v2[1] && v1[0] < v2[0]) return true;
+
+    return false;
+}
+
+int solution(vector<vector<int>> routes) {
+    int n = routes.size();
+
+    sort(routes.begin(), routes.end(), Compare);
+    
+    int answer = 1;
+    int curr = routes[0][1];
+    int i = 1; // 두 번째부터 시작 
+
+    while (i < n)
+    {
+        if (routes[i][0] > curr) {
+            curr = routes[i][1];
+            answer++;
+        }
+
+        i++;
+    }
+
+
+    return answer;
+}
