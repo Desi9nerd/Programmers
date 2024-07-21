2b7a162a847fa140ce25c4f5d1d31991f387ead8
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2024-07-21 20:40:14 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.22 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -4,37 +4,44 @@
 #include <algorithm>
 using namespace std;
 
-// 비교 함수 객체
 struct Compare {
-    bool operator()(const vector<int>& a, const vector<int>& b) {
-        return a[1] > b[1]; // 소요 시간이 작은 순으로 정렬
+    bool operator()(const vector<int>& a, const vector<int>& b){
+        if (a[1] == b[1]) return a[0] > b[0];
+        return a[1] > b[1];
     }
 };
 
 int solution(vector<vector<int>> jobs) {
     int n = jobs.size();
-    sort(jobs.begin(), jobs.end()); // 요청 시간 기준으로 정렬
-
+    sort(jobs.begin(), jobs.end());
+    
     priority_queue<vector<int>, vector<vector<int>>, Compare> pQ;
-    int answer = 0, i = 0, time = 0;
-
-    while (i < jobs.size() || !pQ.empty()) 
+    
+    int time = 0, total = 0, idx = 0;
+    while (idx < n || !pQ.empty())
     {
-        // 현재 시간에서 실행 가능한 모든 작업을 큐에 넣음
-        while (i < jobs.size() && time >= jobs[i][0]) {
-            pQ.push(jobs[i++]);
+        for (int i = idx; i < n; i++){
+            if (jobs[i][0] <= time){
+                pQ.push(jobs[i]);
+                idx++;
+            }
+            else{
+                break;
+            }
         }
-
-        if (!pQ.empty()) { 
-            vector<int> job = pQ.top();
+        
+        if (!pQ.empty()) // 대기 중인 작업이나 현재 바로 시작할 작업이 있는 경우 
+        {
+            time += pQ.top()[1];
+            total += time - pQ.top()[0];
             pQ.pop();
-            time += job[1];              // 작업 소요 시간을 더함
-            answer += time - job[0];     // 작업 종료 시간 - 작업 요청 시간 = 대기 시간
-        } 
-        else {
-            time = jobs[i][0];           // 다음 작업의 요청 시간으로 시간 설정
         }
+        else { // 현재 time 기준 시작할 수 있는 작업이 없는 경우 
+            if (idx < n) {
+                time = jobs[idx][0];  
+            } 
+        }        
     }
-
-    return answer / n; // 평균 대기 시간 반환
+    
+    return total / n;
 }
