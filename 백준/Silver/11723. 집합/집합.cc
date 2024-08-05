d1141679cdd0c63cb6ae7ddde9c227ca6a67e00d
Desi9nerd <97824341+Desi9nerd@users.noreply.github.com>
2024-08-05 11:30:46 +0900

    [Silver V] Title: 집합, Time: 552 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,54 +1,49 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
- 
+
+int m;
+int answer;
+
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    
-    string str = "";
-    int m, x;
-    int arr[21] = {0};
-    
-    cin >> m;
-    for(int i = 0; i < m; i++){
-        cin >> str;
-        
-        if(str=="add"){
-            cin >> x;
-            if(arr[x]==0){ 
-                arr[x] = 1;
-            }
-        }
-        else if(str=="remove"){
-            cin >> x;
-            if(arr[x] == 1){
-                arr[x] = 0;
-            }            
-        }
-        else if(str=="check"){
-            cin >> x;
-            if(arr[x] == 0)  
-                cout << "0\n";
-            else
-                cout << "1\n";
-        }
-        else if(str=="toggle"){
-           cin >> x;
-            if(arr[x] == 1) 
-                arr[x] = 0;
-            else
-                arr[x] = 1;
-        }
-        else if(str=="all"){
-            for(int k = 1; k<=20; k++){ 
-                arr[k] = 1;
-            }            
-        }
-        else if(str=="empty"){
-            memset(arr, 0, sizeof(arr));
-        }
-    }
-    
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	cin >> m;
+	string input;
+	int x;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> input;
+
+		if (input == "add"){
+			cin >> x;
+			answer |= (1 << x);
+		}
+		else if (input == "remove"){
+			cin >> x;
+			answer &= ~(1 << x);
+		}
+		else if (input == "check") {
+			cin >> x;
+
+			if (answer & (1 << x)){
+				cout << "1" << "\n";
+			}
+			else {
+				cout << "0" << "\n";
+			}
+		}
+		else if (input == "toggle") {
+			cin >> x;
+			answer ^= (1 << x);
+		}
+		else if (input == "all") {
+			answer |= ((1 << 21) - 1);
+		}
+		else if (input == "empty") {
+			answer &= ~((1 << 21) - 1);
+		}
+	}
+
+	return 0;
 }
